2c08bd3da5f3724673f10445011333301317c717
faizaAman-2 <109220998+faizaAman-2@users.noreply.github.com>
2023-03-13 01:30:48 +0500

    payment
    payment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const cors = require('cors')
+const stripe=require('stripe')("sk_test_51Mj9rnCgjKRoygoHFoS8msjcqo5M8T6TPeAsJHC4KqpVWYWC53mIlMnkR1wkXld10uLv3s7SylFoJxRaRzMhfPfZ00pjWDIzGb")
 var bodyParser = require('body-parser')
+const uuid=require("uuid").v4
 const userRouter=require ('./db/routes/user')
 const addpost=require ('./db/routes/post')
 const nutritionProfiles=require ('./db/routes/nutritionprofile')
@@ -16,5 +18,44 @@ app.use(addpost);
 app.use(nutritionProfiles);
 app.use(Query);
 app.use(Track);
+
+app.post('/checkout',async (req,res)=>{
+    console.log("Request:",req.body);
+    let error,status
+    try {
+      const {nut,token}=req.body
+      const customer=await stripe.customers.create({
+        email:token.email,
+        source:token.id
+      })
+      const key=uuid()
+      const charge = await stripe.charges.create({
+        amount: nut.payment * 100,
+        currency: "usd",
+        customer: customer.id,
+        receipt_email: token.email,
+        description: `paid the ${nut.name}`,
+        shipping: {
+          name: token.card.name,
+          address: {
+            line1: token.card.address_line1,
+            line2: token.card.address_line2,
+            city: token.card.address_city,
+            country: token.card.address_country,
+            postal_code: token.card.address_zip,
+          },
+        },
+      });
+      
+      console.log("payment:",{charge});
+      status="success";
+    } 
+    catch (error) {
+
+      console.log("Error:",error)
+      status="failure";
+    }
+    res.json({error,status});
+})
 
 app.listen(8000)
